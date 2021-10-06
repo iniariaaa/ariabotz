3c2ad21c07f22b28d1fe1d09c88801144f1b29c1
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-10-07 06:26:31 +0700

    Create asupan.js

---
+++ plugins/asupan.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Bentar Kak...')
+  let res = `https://ariarestapii.herokuapp.com/api/asupan?apikey=aria`
+  conn.sendFile(m.chat, res, 'asupan.mp4', `Ini Kak`, m, false)
+}
+handler.help = ['asupan'].map(v => v + ' ')
+handler.tags = ['image']
+
+handler.command = /^(asupan)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
